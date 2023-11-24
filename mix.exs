703968b896c118424dbef478f0ade57c9218e2ed
Dominic Letz <dominic.letz@berlin.de>
2023-11-24 18:05:54 +0100

    v2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Socket.Mixfile do
   def project do
     [
       app: :socket,
-      version: "2.0.0",
+      version: "2.0.1",
       deps: deps(),
       package: package(),
       description: "Socket handling library for Elixir, updated for OTP20+"
